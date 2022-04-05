8d5ad440401e019de48cd81c44ac81febf07f31a
vidhulkv731 <100456113+vidhulkv731@users.noreply.github.com>
2022-04-05 06:41:25 +0530

    file uploaded

---
+++ calculator/calc.js
@@ -0,0 +1,16 @@
+var screen = document.getElementById('screen');
+
+function btnClick(value){
+    screen.value += value;
+
+}
+
+function clearscreen(){
+    screen.value="";
+
+}
+
+function findResult(){
+    var result = eval(screen.value)
+    screen.value = result;
+}
